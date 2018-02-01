a7cc2389d10f6db6ed003b273785ba630f792829
jiawei2018 <35362950+jiawei2018@users.noreply.github.com>
2018-01-31 21:17:19 -0800

    Add files via upload

---
+++ Earth.java
@@ -0,0 +1,18 @@
+
+public class Earth {
+
+	public static void main(String[] args) {
+		
+		Human tom;
+		tom=new Human();
+		
+		tom.age=6;
+		tom.eyeColor="blue";
+		tom.heightInInches=54;
+		tom.name="Tom Lee";
+		
+		tom.Speak();
+				
+	}
+	
+}
